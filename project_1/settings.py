c9d30f2f08c1f58597cee78c180241d6da3bdda5
Siddhesh-Shukla <siddheshshukla1024@gmail.com>
2021-12-31 09:06:21 +0530

    User Added

--- project_1/settings.py
+++ project_1/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -121,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PAXK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'blog-home'
