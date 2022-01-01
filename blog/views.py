cec545a8866474ea204ac8aedcc8c4fcdcf8c4bf
Siddhesh-Shukla <siddheshshukla1024@gmail.com>
2022-01-01 07:41:38 +0530

    CRUD added

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,64 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ( 
+    ListView, 
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
-posts = [
-    {
-        'title': 'Title1',
-        'author': 'author1'
-    },
-    {
-        'title': 'Title2',
-        'author': 'author2'
-    }
-]
+# def home(request):
+#     context = {
+#         'posts': Post.objects.all(),
+#         'title': 'NEW TITLE'
+#     }
+#     return render(request, 'blog/blog.html', context)
 
-def home(request):
-    context = {
-        'posts': Post.objects.all(),
-        'title': 'NEW TITLE'
-    }
-    return render(request, 'blog/blog.html', context)
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/blog.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    # <app>/<model>_<viewtype>.html
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_delete.html'
+    success_url = '/blog'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
